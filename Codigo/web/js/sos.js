ecbb75985c2e0658e0447ca6a1438d0ce737bc28
yzPeedro <pedrocruzpessoa16@gmail.com>
2021-10-02 09:38:56 -0300

    inserindo codigo da pagina inicial do projeto na versão web

---
+++ Codigo/web/js/sos.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+}
+
+function goBack() {
+    window.history.back();
+}
