17b4fe3b8badffa19a2394d3829f681785df260d
Samara <samara.ferreira@atsinformtica.com.br>
2021-11-19 00:25:46 -0300

    Máscara do site - Parte 2

--- Codigo/web/js/inicio.js
+++ Codigo/web/js/inicio.js
@@ -1,5 +1,11 @@
-// function meuRelogio() {
-//     setTimeout( window.open()
-//     }, 4000);
-//   }
+window.onload = () => {
+    setTimeout(function () {
+        if (document.URL == 'http://127.0.0.1:5500/Codigo/web/pages/Inicio.html') {
+            window.location.href = "../pages/Inicio2.html";
+        }
+        else{
+           window.location.href = "../pages/Inicio.html";   
+        }
+    }, 5000);
+}
 
