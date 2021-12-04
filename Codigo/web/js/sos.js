3efcc51d8cdf2f740364b78a80762c4ab1fecd1e
yzPeedro <pedrocruzpessoa16@gmail.com>
2021-12-03 22:42:54 -0300

    colocando contatos na pagina de sos

--- Codigo/web/js/sos.js
+++ Codigo/web/js/sos.js
@@ -12,6 +12,7 @@ function goBack() {
 
 function openModal() {
     $("#modal").show();
+    this.getContacts();
 }
 
 function closeModal() {
@@ -22,5 +23,26 @@ function closeModal() {
 window.onclick = function(event) {
     if (event.target == modal) {
         $("#modal").hide();
+    }
+}
+
+function getContacts()
+{
+    let contacts;
+
+    if(JSON.parse(localStorage.db_contato).data.length > 0) {
+        contacts = JSON.parse(localStorage.db_contato).data;
+
+        contacts.forEach(contact => {
+            document.querySelector("#modal-contacts").innerHTML += `
+                <hr>
+                <div class='col-md-12'>
+                    <b>Nome: </b><span>${contact.nome}</span><br>
+                    <b>Telefone contactado: </b><span>${contact.telefone}</span>
+                </div>
+            `;
+        });
+    } else {
+        document.querySelector("#modal-contacts").innerHTML = "<b style='color: red'>Nenhum contato selecionado.</b>";
     }
 }
