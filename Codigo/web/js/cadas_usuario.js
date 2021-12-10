96875b6a499f1a64017179f093ad23984edd7a55
Mayara <mayararoquerangel@gmail.com>
2021-12-09 23:16:38 -0300

    Mudanças no código de tela de login e cadastro

--- Codigo/web/js/cadas_usuario.js
+++ Codigo/web/js/cadas_usuario.js
@@ -1,37 +1,83 @@
-function processaForm(evt){
-    evt.preventDefault ();
-        d_nome = window.document.getElementById('nome').value;
-        d_senha = window.document.getElementById('senha').value;
-        d_tel = window.document.getElementById('telefone').value;
-        d_data = window.document.getElementById('data').value;
-        d_cpf = window.document.getElementById('cpf').value;
-        d_rua = window.document.getElementById('rua').value;
-        d_bairro = window.document.getElementById('bairro').value;
-        d_cidade = window.document.getElementById('cidade').value;
-        d_comple = window.document.getElementById('comple').value;
-    
-        let usuario = {
-            nome : d_nome,
-            senha : d_senha,
-            tel : d_tel,
-            data : d_data,
-            cpf : d_cpf,
-            rua : d_rua,
-            bairro : d_bairro,
-            cidade : d_cidade,
-            comple : d_comple,
-        }
-        
-        // Verifica se ja existe algo no local storage
-        let usuarios = []
-        let usuarios_txt = localStorage.getItem("db_usuario");
-        if (usuarios_txt) {
-            usuarios = JSON.parse(usuarios_txt)
-        }
+let usuarios_txt = localStorage.getItem("db_usuario");
+
+if (usuarios_txt) {
+    usuarios = JSON.parse(usuarios_txt)
+}
+
+function processaForm(evt) {
+    evt.preventDefault();
+    d_nome = window.document.getElementById('nome').value;
+    d_senha = window.document.getElementById('senha').value;
+    d_tel = window.document.getElementById('telefone').value;
+    d_data = window.document.getElementById('data').value;
+    d_cpf = window.document.getElementById('cpf').value;
+    d_rua = window.document.getElementById('rua').value;
+    d_bairro = window.document.getElementById('bairro').value;
+    d_cidade = window.document.getElementById('cidade').value;
+    d_comple = window.document.getElementById('comple').value;
+
+    let usuario = {
+        nome: d_nome,
+        senha: d_senha,
+        tel: d_tel,
+        data: d_data,
+        cpf: d_cpf,
+        rua: d_rua,
+        bairro: d_bairro,
+        cidade: d_cidade,
+        comple: d_comple,
+    }
+
+    // Verifica se ja existe algo no local storage
+    let usuarios = []
+    let usuarios_txt = localStorage.getItem("db_usuario");
+
+    if (usuarios_txt) {
+        usuarios = JSON.parse(usuarios_txt)
+    }
+    if (usuarios.filter(u => u.cpf === usuario.cpf).length > 0) {
+        window.alert("CPF JÁ CADASTRADO!")
+    } else {
         usuarios.push(usuario)
         localStorage.setItem("db_usuario", JSON.stringify(usuarios))
         window.alert("Dados enviados com sucesso!");
+        window.location.href = "/login.html";
     }
 
- 
+
+}
+function updateContato(id, contato) {
+    console.log(id)
+    console.log(usuarios)
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = usuarios.map((obj, index) => index)[0];
+    console.log(index)
+
+    // Altera os dados do objeto no array
+    usuarios[index].nome = contato.nome,
+        usuarios[index].tel = contato.tel,
+        usuarios[index].data = contato.data,
+        usuarios[index].cidade = contato.cidade,
+        usuarios[index].cpf = contato.cpf,
+        usuarios[index].bairro = contato.bairro,
+        usuarios[index].comple = contato.comple,
+        usuarios[index].rua = contato.rua
+
+    displayMessage("Contato alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_usuario', JSON.stringify(db));
+}
+
+function deleteContato(id) {
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    displayMessage("Contato removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_usuario', JSON.stringify(db));
+}
+
+
 
